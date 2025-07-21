2bda85d390667ef257d1884f62bcd1dc114fce88
Nikolay Kiryanov <nikolay@kiryanov.ru>
2025-07-21 20:45:39 +0300

    Optimize how integration tests works Now create server on transaction and pass it to inner test. So, now we don't have to recreate server on each test.

--- tests/integration/auth/auth_login_test.go
+++ tests/integration/auth/auth_login_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/require"
 
 	"github.com/nkiryanov/gophermart/internal/testutil"
@@ -23,58 +24,61 @@ func Test_Login(t *testing.T) {
 	pg := testutil.StartPostgresContainer(t)
 	t.Cleanup(pg.Terminate)
 
-	t.Run("login ok", func(t *testing.T) {
-		integration.RunTx(pg.Pool, t, func(srvURL string, s integration.Services) {
-			_, err := s.AuthService.Register(t.Context(), "nk", "StrongEnoughPassword")
-			require.NoError(t, err)
+	integration.ServeWithTx(pg.Pool, t, func(tx pgx.Tx, srvURL string, s integration.Services) {
+		_, err := s.AuthService.Register(t.Context(), "nk", "StrongEnoughPassword")
+		require.NoError(t, err)
 
-			data := `{"login": "nk", "password": "StrongEnoughPassword"}`
-			resp, err := http.Post(srvURL+LoginURL, "application/json", strings.NewReader(data))
-			require.NoError(t, err)
-			body, err := io.ReadAll(resp.Body)
-			require.NoError(t, err)
-			defer func() { _ = resp.Body.Close() }()
+		t.Run("login ok", func(t *testing.T) {
+			testutil.WithTx(tx, t, func(_ pgx.Tx) {
+				data := `{"login": "nk", "password": "StrongEnoughPassword"}`
+				resp, err := http.Post(srvURL+LoginURL, "application/json", strings.NewReader(data))
+				require.NoError(t, err)
+				body, err := io.ReadAll(resp.Body)
+				require.NoError(t, err)
+				defer func() { _ = resp.Body.Close() }()
 
-			require.Equalf(t, http.StatusOK, resp.StatusCode, "not expected code. Body: %s", string(body))
-			require.JSONEq(t, `
-				{
-					"message": "User logged in successfully"
-				}`, string(body))
+				require.Equalf(t, http.StatusOK, resp.StatusCode, "not expected code. Body: %s", string(body))
+				require.JSONEq(t, `
+					{
+						"message": "User logged in successfully"
+					}`, string(body))
 
-			require.Equal(t, 1, len(resp.Cookies()))
-			cookie := resp.Cookies()[0]
-			require.Equal(t, "refreshtoken", cookie.Name)
-			require.Equal(t, cookie.HttpOnly, true, "refresh cookie should be HttpOnly")
-			require.Equal(t, "/", cookie.Path, "refresh cookie should be available on / path")
-			require.Equal(t, http.SameSiteStrictMode, cookie.SameSite, "refresh cookie should be SameSite Strict")
-			require.InDelta(t, (24 * time.Hour).Seconds(), cookie.MaxAge, 1, "max age should be refresh TTL with 1 second delta")
-			require.NotEmpty(t, cookie.Value, "refresh cookie should not be empty")
+				require.Equal(t, 1, len(resp.Cookies()))
+				cookie := resp.Cookies()[0]
+				require.Equal(t, "refreshtoken", cookie.Name)
+				require.Equal(t, cookie.HttpOnly, true, "refresh cookie should be HttpOnly")
+				require.Equal(t, "/", cookie.Path, "refresh cookie should be available on / path")
+				require.Equal(t, http.SameSiteStrictMode, cookie.SameSite, "refresh cookie should be SameSite Strict")
+				require.InDelta(t, (24 * time.Hour).Seconds(), cookie.MaxAge, 1, "max age should be refresh TTL with 1 second delta")
+				require.NotEmpty(t, cookie.Value, "refresh cookie should not be empty")
 
-			require.Contains(t, resp.Header, "Authorization")
-			header := resp.Header.Get("Authorization")
-			require.Contains(t, header, "Bearer")
+				require.Contains(t, resp.Header, "Authorization")
+				header := resp.Header.Get("Authorization")
+				require.Contains(t, header, "Bearer")
+
+			})
 		})
-	})
 
-	t.Run("login failed", func(t *testing.T) {
-		integration.RunTx(pg.Pool, t, func(srvURL string, _ integration.Services) {
-			data := `{"login": "nk", "password": "WrongPassword"}`
+		t.Run("login failed", func(t *testing.T) {
+			testutil.WithTx(tx, t, func(_ pgx.Tx) {
+				data := `{"login": "nk", "password": "WrongPassword"}`
 
-			resp, err := http.Post(srvURL+LoginURL, "application/json", strings.NewReader(data))
-			require.NoError(t, err)
-			body, err := io.ReadAll(resp.Body)
-			require.NoError(t, err)
-			defer func() { _ = resp.Body.Close() }()
+				resp, err := http.Post(srvURL+LoginURL, "application/json", strings.NewReader(data))
+				require.NoError(t, err)
+				body, err := io.ReadAll(resp.Body)
+				require.NoError(t, err)
+				defer func() { _ = resp.Body.Close() }()
 
-			require.Equalf(t, http.StatusUnauthorized, resp.StatusCode, "not expected code. Body: %s", string(body))
-			require.JSONEq(t, `
-				{
-					"error": "service_error",
-					"message": "User not found"
-				}`, string(body))
+				require.Equalf(t, http.StatusUnauthorized, resp.StatusCode, "not expected code. Body: %s", string(body))
+				require.JSONEq(t, `
+					{
+						"error": "service_error",
+						"message": "User not found"
+					}`, string(body))
 
-			require.Equal(t, 0, len(resp.Cookies()), "no cookies should be set on login error")
-			require.NotContains(t, resp.Header, "Authorization", "Authorization header should not be set")
+				require.Equal(t, 0, len(resp.Cookies()), "no cookies should be set on login error")
+				require.NotContains(t, resp.Header, "Authorization", "Authorization header should not be set")
+			})
 		})
 	})
 }
